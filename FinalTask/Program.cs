3af87296c3f4d5b4c59873f3b41c0d0aadac1f14
Khoroltseva.Yulia <yulia.nosovets@mail.ru>
2023-03-01 05:10:32 +0300

    Программа написана

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -3,3 +3,36 @@
 // меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте 
 // выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+// Примеры:
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> [ "-2" ]
+// ["Russia", "Denmark", "Kazan"] -> [ ]
+
+string[] array = new string[] {"it", "hello", "5674", "is", "world", "ok", "12598"};
+string[] newlist = new string[array.Length];
+void NewArray(string[] array, string[] newlist)
+{
+    int count = 0;
+    int i=0;
+    while (i < array.Length )
+    {
+       if(array[i].Length < 4 & array[i].Length >0)
+       {
+        newlist[count] = array[i]; 
+        count++;
+       }
+       i++;
+    }
+}
+void PrintArray(string[] array)
+{
+
+    Console.Write($"->[ {array[0]}");
+    for (int i = 1; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]}");
+    }
+    Console.WriteLine("]");
+}
+NewArray(array, newlist);
+PrintArray(newlist);
